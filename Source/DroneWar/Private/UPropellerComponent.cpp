b9f6539c51773c9891816f7f92e6f03b4d9e7e7a
이지은 <71183687+Jinnycorn@users.noreply.github.com>
2025-06-27 00:15:23 +0900

    PropellerComp 생성
    왜 USceneComponent로 상속 받나?
    - 위치와 회전이 필요하기 때문
    - 드론에 붙이는 방식 때문(USceneComponent는 Hierarchy 트리에 붙일 수 있음)
    - 기능은 있지만 액터가 필요 없는 대상이기 때문

---
+++ Source/DroneWar/Private/UPropellerComponent.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UPropellerComponent.h"
+
+// Sets default values for this component's properties
+UUPropellerComponent::UUPropellerComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// PropellerMesh 생성 및 부착
+	PropellerMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PropellerMesh"));
+	PropellerMesh->SetupAttachment(this);
+
+	// 충돌, 물리 해제 (회전용 비주얼일 뿐)
+	PropellerMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	PropellerMesh->SetSimulatePhysics(false);
+}
+
+
+// Called when the game starts
+void UUPropellerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UUPropellerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
